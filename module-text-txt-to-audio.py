09cada12c810f3ad6686998367c5e57a64774073
Kauan Alves <107011580+kauannzz@users.noreply.github.com>
2022-09-20 22:34:43 -0300

    Update module-text-txt-to-audio.py

--- module-text-txt-to-audio.py
+++ module-text-txt-to-audio.py
@@ -1,9 +1,9 @@
 from cgitb import text
+from msilib.schema import Error
+from multiprocessing.sharedctypes import Value
 from gtts import gTTS
 
-
 class textToVoice():
-    
     def manualVoice():
         myText = input('Digite seu texto, meu amigo: ')
         archiveName = input('Digite o nome do arquivo a ser salvo: ')
@@ -16,7 +16,6 @@ class textToVoice():
         print('O arquivo foi salvo com sucesso!')
 
     def txtToVoice():
-        
         language = 'pt-BR'
 
         nameArchive = input('Digite o nome do seu arquivo .TXT: ')
@@ -30,3 +29,10 @@ class textToVoice():
         print('O arquivo foi salvo com sucesso!')
             
         
+select = input('Qual módulo você quer usar?\n(1) Manual\n(2) TXT para voz')
+if select == 1:
+    textToVoice.manualVoice()
+elif select == 2:
+   textToVoice.txtToVoice()
+else:
+   print('Você não digitou um número ou escolheu uma opção inválida!')
